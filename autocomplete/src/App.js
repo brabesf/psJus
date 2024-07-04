47286485e0e7d7d55e95b00891a392925c9c8f95
brabesf <lipebrabes@gmail.com>
2024-07-04 14:52:45 -0300

    Inserir botao de busca

--- autocomplete/src/App.js
+++ autocomplete/src/App.js
@@ -1,6 +1,6 @@
 import logo from './resources/jusbrasil.png'
 import './App.css';
-import {TextField} from '@material-ui/core'
+import {TextField, Button} from '@material-ui/core'
 import { useState } from 'react';
 
 function App() {
@@ -14,18 +14,25 @@ function App() {
         <div className='Buscador'>
           <a className = "Title">Busca com autocompletar</a>
           <p className = "Subtitle">Digite no campo abaixo para exibir sugetões</p>
-          <TextField 
-          label='Procure em Jusbrasil'
-          className="Text-field"
-          variant='filled'
-          size='small'
-          InputProps={{
-            style: {
-              backgroundColor: '#fff', // Change this to your desired color
-              minWidth: '35vw'
-            },
-          }}
-          onChange={(event) => setQuery(event.target.value)}/>
+          <div className='Input'>
+            <TextField 
+            label='Procure em Jusbrasil'
+            className="Text-field"
+            variant='filled'
+            size='small'
+            InputProps={{
+              style: {
+                backgroundColor: '#fff', // Change this to your desired color
+                minWidth: '35vw'
+              },
+            }}
+            onChange={(event) => setQuery(event.target.value)}/>
+            <Button style={{backgroundColor: '#5555ff',
+                            color: 'white',
+                            flex: '1'
+            }} 
+            variant='contained'>BUSCAR</Button>
+          </div>
         </div>
       </header>
 

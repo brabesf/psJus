0c3095850c36f56747210b270d8fe8db9e29fbd5
brabesf <lipebrabes@gmail.com>
2024-07-09 00:40:44 -0300

    Fix - Usando LazyQuery hook

--- autocomplete/src/Autosuggest.js
+++ autocomplete/src/Autosuggest.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useQuery, gql } from '@apollo/client';
+import { useLazyQuery, gql } from '@apollo/client';
 import {TextField, Button} from '@material-ui/core'
 import { useState } from 'react';
 import './styles/Autosuggest.css';
@@ -30,73 +30,80 @@ function Autosuggest() {
   }
 `;
 
-const SET_QUERY = gql`
-query GetMatches($text: String!) {
-  getMatches(text: $text)
-}
-`;
-  
-const handleClick = (text) => {
-  setQuery(text)
+const [getMatches, { loading, error, data }] = useLazyQuery(GET_MATCHES);
 
+const handleClick = (text) => {
+  setQuery(text);
 };
 
-const { loading, error, data } = useQuery(GET_MATCHES, {
-  variables: { text: query },
-});
+const handleSearch = () => {
+  getMatches({ variables: { text: query } });
+};
+
+const handleType = (event) => {
+  setQuery(event.target.value);
+  getMatches({ variables: { text: event.target.value } });
+}
 
 if (error) return (
   <div>
     <p>Error :(</p>
-    <p>{error.message}(</p>
+    <p>{error.message}</p>
   </div>
 );
 
 return (
-<div>
-  <div className='Input'>
-    <TextField 
-    value={query}
-    label='Procure em Jusbrasil'
-    className="Text-field"
-    variant='filled'
-    size='small'
-    InputProps={{
-      style: {
-        backgroundColor: '#fff', // Change this to your desired color
-        width: '50vw'
-      },
-    }}
-    onChange={(event) => (setQuery(event.target.value))}/>
-
-    <Button style={{backgroundColor: '#5555ff',
-                  color: 'white',
-                  
-                  width: '5vw'
-    }} 
-    variant='contained'>BUSCAR</Button>
+  <div>
+    <div className='Input'>
+      <TextField 
+        value={query}
+        label='Procure em Jusbrasil'
+        className="Text-field"
+        variant='filled'
+        size='small'
+        InputProps={{
+          style: {
+            backgroundColor: '#fff',
+            width: '50vw'
+          },
+        }}
+        onChange={handleType}
+      />
+      <Button
+        style={{
+          backgroundColor: '#5555ff',
+          color: 'white',
+          width: '5vw'
+        }}
+        variant='contained'
+        onClick={handleSearch}
+      >
+        BUSCAR
+      </Button>
+    </div>
+    <div className='Opcoes'>
+      {loading ? (
+        <p></p>
+      ) : (
+        data && data.getMatches.map((text, index) => {
+          const firstPart = text.substring(0, query.length);
+          const lastPart = text.substring(query.length);
+          
+          return (
+            <OptionButton 
+              key={index}
+              onClick={() => handleClick(text)}
+              variant='contained'
+            >
+              <span style={{ fontWeight: 700, whiteSpace: 'pre' }}>{firstPart}</span>
+              <span style={{ fontWeight: 400 , whiteSpace: 'pre'}}>{lastPart}</span>
+            </OptionButton>
+          )
+        })
+      )}
+    </div>
   </div>
-
-  <div className='Opcoes'>{loading?(
-    <p></p>
-    ) : (
-    data.getMatches.map((text, index) => {
-      const firstPart = text.substring(0, query.length);
-      const lastPart = text.substring(query.length);
-      
-      return (
-        <OptionButton 
-        onClick={() => handleClick(text)}          
-        variant='contained'>
-          <span style={{ fontWeight: 700, whiteSpace: 'pre' }}>{firstPart}</span>
-          <span style={{ fontWeight: 400 , whiteSpace: 'pre'}}>{lastPart}</span>
-        </OptionButton>
-      )
-
-    })
-  )}
-  </div>
-</div>);
+);
 }
 
 export default Autosuggest;
